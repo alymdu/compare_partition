8032ff70015623a97d5ae207971b49f8fc2848bd
Alymbek Damir Uulu <alymdu@gmail.com>
2023-04-17 18:42:05 -0700

    Add new line

--- src/toolset.py
+++ src/toolset.py
@@ -2,12 +2,12 @@ import mysql.connector
 
 from options import Options
 
-class Tool:
 
+class Tool:
     def __init__(self, options: Options, connector=mysql.connector):
         self.mysql_connector = connector
         self.options = options
-    
+
     def connect(self):
         self.conn = self.mysql_connector.connect(
             user=self.options.username,
@@ -16,7 +16,7 @@ class Tool:
             port=self.options.port,
             database=self.options.database,
         )
-        
+
     def query(self, query, param):
         cur = self.conn.cursor()
         cur.execute(query, param)
